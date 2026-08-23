5a509dfdc1759bc81036f3393ddb51106473f85f
Anton Sivashtenko <siffash@gmail.com>
2026-08-23 14:19:50 +0300

    remove vite-tsconfig-paths

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,23 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tsconfigPaths({
-      projects: [path.resolve(__dirname, "../tsconfig.json")],
-    }),
-  ],
+  plugins: [react()],
   base: "/transliterate-any-to-any/",
   resolve: {
     alias: {
       "transliterate-any-to-any": path.resolve(__dirname, "../dist/index.mjs"),
     },
+    tsconfigPaths: true,
   },
   worker: {
     format: "es",
